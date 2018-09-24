42f004902dcc0e9fd418e439e56209b2b3c7c4dd
wangz10 <wangzc921@gmail.com>
2018-09-24 07:48:40 -0400

    Finished hw2 report

--- hw2/problem8.sh
+++ hw2/problem8.sh
@@ -8,13 +8,15 @@ for b in 10000 30000 50000; do
 done
 
 
-b=50000
-r=0.02
+# b=50000
+# r=0.02
+b=30000
+r=0.01
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 1 \
-   -l 2 -s 32 -b $b -lr $r --exp_name hc_base
+   -l 2 -s 32 -b $b -lr $r --exp_name "hc_base_b"$b
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 1 \
-   -l 2 -s 32 -b $b -lr $r -rtg --exp_name hc_rtg
+   -l 2 -s 32 -b $b -lr $r -rtg --exp_name "hc_rtg_b"$b
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 1 \
--l 2 -s 32 -b $b -lr $r --nn_baseline --exp_name hc_nn
+-l 2 -s 32 -b $b -lr $r --nn_baseline --exp_name "hc_nn_b"$b
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.9 -n 100 -e 1 \
-   -l 2 -s 32 -b $b -lr $r -rtg --nn_baseline --exp_name hc_rtg_nn
+   -l 2 -s 32 -b $b -lr $r -rtg --nn_baseline --exp_name "hc_rtg_nn_b"$b
